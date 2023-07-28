88e688a6e8d3eeb78b44f2d4ebc2e9ee56432ea9
pobrelkey <pobrelkey@users.noreply.github.com>
2023-07-29 00:01:20 +0100

    many bells and whistles added to correcthorse.rb

--- correcthorse.rb
+++ correcthorse.rb
@@ -6,6 +6,8 @@
 #
 
 require 'optparse'
+require 'securerandom'
+require 'set'
 
 
 # 1296 words from the short variant of the EFF's Diceware lists:
@@ -129,25 +131,220 @@ WORDS = %w(
 	yield yodel yoga zen zit yummy zebra zero zesty zippy zone zoom
 )
 
+DIGITS_RX = /^(.+?)([aeios])/
+
+
+# Random password selection engine - complicated, but has to be if:
+# 1. we want all possible random passwords to be equally likely
+#    (thereby containing the same amount of entropy)
+# 2. we want to be able to say how many possible passwords there are
+#    (i.e. how many bits of entropy does the password have)
+# This becomes more complicated the more constraints/tweaks you add
+# (prefixes, acrostics, digit manipulation, etc.)
+
+class CorrectHorse
+
+	def initialize(words=WORDS, 
+			bit_count=nil, max_length=nil, word_count=nil,
+			prefix=nil, prefix_frac=0.5, acrostic=nil,
+			caps=false, digitify=false,
+			joiner=' ')
+		@bit_count = bit_count
+		@max_length = max_length
+		@word_count = word_count
+		@prefix = prefix
+		@prefix_frac = prefix_frac
+		@acrostic = acrostic
+		@caps = caps
+		@digitify = digitify
+		@joiner = joiner
+		@words = words
+		@min_word_length = words.collect{|x| x.length }.min
+
+		@state_map = create_markov_tree()
+		@count_cache = {}
+		@possible_outcomes = outcome_count(@state_map,@count_cache)
+		raise ArgumentError.new('no possible outcomes given specified options') if @possible_outcomes == 0
+	end
+
+private
+	ROOT_STATE='*'
+
+	def transition(next_word, old_state=ROOT_STATE)
+		new_state = (old_state == ROOT_STATE) ?  {} : old_state.dup
+		new_state[:count] = (new_state[:count] || 0) + 1 if !@word_count.nil? || !@prefix.nil? || !@bit_count.nil?
+		return nil if !@word_count.nil? && new_state[:count] > @word_count
+		return nil if !@acrostic.nil? && @acrostic[new_state[:count]-1] != next_word[0]
+		if !@prefix.nil?
+			return nil if old_state == ROOT_STATE && !next_word.start_with?(@prefix)
+			new_state[:prefixed_count] = (new_state[:prefixed_count] || 0) + (next_word.start_with?(@prefix) ? 1 : 0)
+		end
+		new_state[:digitable] = new_state[:digitable] || (next_word =~ DIGITS_RX ? true : false) if @digitify
+		if !@max_length.nil?
+			new_state[:length] = old_state == ROOT_STATE ? next_word.length : (new_state[:length] + @joiner.length + next_word.length)
+			return nil if new_state[:length] > @max_length
+		end
+		return new_state.freeze
+	end
+
+	def is_acceptable_end_state(state)
+		return false if state == ROOT_STATE
+		return false if !@word_count.nil? && state[:count] != @word_count
+		return false if @digitify && !state[:digitable]
+		return false if !@prefix.nil? && state[:prefixed_count] < state[:count] * @prefix_frac
+		return false if !@max_length.nil? && state[:length] > @max_length
+		return true
+	end
+
+	def has_further_states(state)
+		return true if state == ROOT_STATE
+		return false if !@max_length.nil? && state[:length] > @max_length - @joiner.length - @min_word_length
+		return false if !@word_count.nil? && state[:count] == @word_count
+		return true
+	end
+
+	def outcome_count(state_map, cache={}, from_state=ROOT_STATE)
+		return cache[from_state] if cache.has_key?(from_state)
+		transition_map = state_map[from_state]
+		return cache[from_state] = (is_acceptable_end_state(from_state) ? 1 : 0) if transition_map.nil? || transition_map.empty?
+		return cache[from_state] = transition_map.keys.inject(0){|a,b| a + transition_map[b] * outcome_count(state_map,cache,b) }
+	end
+
+	def create_markov_tree()
+		# if @bit_count in use, build up a Markov tree one layer at a time until
+		#    log_2(number of possible outcomes) >= @bit_count
+		# otherwise construct a Markov tree to exhaustion of possible states
+		state_map = {}
+		new_states = nil
+		while new_states.nil? || (!@bit_count.nil? ? (Math.log(outcome_count(state_map),2) < @bit_count) : !new_states.empty?)
+			old_states = new_states || [ROOT_STATE]
+			new_states = Set.new
+		   	old_states.each do |old_state|
+				transition_map = {}
+				@words.each do |word|
+					new_state = transition(word, old_state)
+					next if new_state.nil?
+					transition_map[new_state] = (transition_map[new_state] || 0) + 1
+				end if has_further_states(old_state)
+				new_states = new_states.union(transition_map.keys)
+				state_map[old_state] = transition_map
+			end
+			new_states = new_states - old_states
+		end
+		return state_map.freeze
+	end
+
+public
+
+	attr_reader :possible_outcomes
+
+	def random_password()
+		# use saved Markov chain stats to pick a password in such a way that all outcomes are equally likely
+		result_words = []
+		current_state = ROOT_STATE
+		until is_acceptable_end_state(current_state)
+			result_words = []
+			current_state = ROOT_STATE
+			bits = SecureRandom.rand(@possible_outcomes)
+			until !@state_map.has_key?(current_state) || @state_map[current_state].empty?
+				@words.each do |word|
+					next_state = transition(word, current_state)
+					next if next_state.nil?
+					state_outcome_count = outcome_count(@state_map, @count_cache, next_state)
+					if bits < state_outcome_count
+						result_words << word
+						current_state = next_state
+						break
+					else
+						bits -= state_outcome_count
+					end
+				end
+			end
+		end
+		result_words.collect!{|x| x.capitalize } if @caps
+		if @digitify
+			index_to_digitify = result_words.each_with_index.collect{|x,i| (x =~ DIGITS_RX) ? i : nil }.compact.max
+			result_words[index_to_digitify] = result_words[index_to_digitify].sub(DIGITS_RX){ $1+$2.tr('aeios','43105') }
+		end
+		return result_words.join(@joiner)
+	end
+
+end
+
+
+
+######## utility functions
+
+def croak(msg)
+	STDERR.print("ERROR: %s\n" % msg)
+	exit(1)
+end
+
 # default number of bits of entropy for the password
 DEFAULT_BITS = 48
+DEFAULT_PREFIX_FRAC = 0.5
 
-bit_count = DEFAULT_BITS
-words = WORDS
 
-OptionParser.new do |opts|
-	opts.banner = "Random passphrase generator in the style of https://xkcd.com/936/\nUsage: #{File.basename(__FILE__)} [options]"
+######## main ########
 
-	opts.on("-b", "--bits BITS", Float, "Bits of entropy in output (default: #{DEFAULT_BITS})") do |v|
-		bit_count = v.to_f
+def main()
+	bit_count = max_length = word_count = prefix = acrostic = nil
+	caps = digitify = verbose = false
+	repeat = 1
+	prefix_frac = DEFAULT_PREFIX_FRAC
+	joiner = ' '
+	words = WORDS
+
+	OptionParser.new do |opts|
+		opts.banner = "Random passphrase generator in the style of https://xkcd.com/936/\nUsage: #{File.basename(__FILE__)} [options]"
+		opts.on('-b', '--bits BITS', Float, "Bits of entropy in output (default: #{DEFAULT_BITS})") {|v| bit_count = v.to_f }
+		opts.on('-l', '--length CHARS', Integer, 'result should be at most this many characters Long') {|v| max_length = v.to_i }
+		opts.on('-w', '--words COUNT', Integer, 'result should be this many Words long') {|v| word_count = v.to_i }
+		opts.on('-c', '--caps', 'Capitalize first letter of each word') { caps = true }
+		opts.on('-d', '--digit', 'change one letter to a Digit') { digitify = true }
+		opts.on('-y', '--hyphens', 'join words together with hYphens') { joiner = '-' }
+		opts.on('-u', '--underscores', 'join words together with Underscores') { joiner = '_' }
+		opts.on('-j', '--join JOINER', String, 'specify Joiner which should appear between words') {|v| joiner = v }
+		opts.on('-m', '--camel', 'join words in caMel-case (implies -c -j \'\')') { caps = true; joiner = '' }
+		opts.on('-s', '--start-with PREFIX', String, 'first word and some fraction of others should Start with PREFIX') {|v| prefix = v.strip.downcase }
+		opts.on('-S', '--start-fraction FRACTION', Float, "what fraction of all words should start with prefix (default: #{DEFAULT_PREFIX_FRAC})") {|v| prefix_frac = v.to_f }
+		opts.on('-a', '--acrostic WORD', String, 'result should be an Acrostic which spells WORD') {|v| acrostic = v.strip.downcase }
+		opts.on('-r', '--repeat COUNT', Integer, 'print this many random passwords') {|v| repeat = v.to_i }
+		opts.on('-f', '--word-file FILE', String, 'File containing words to be chosen randomly') do |v|
+			words = File.read(v).scan(/(?:^|(?<=\s))([a-z]+)(?:$|(?=\s))/).flatten
+		end
+		opts.on('-v', '--verbose', 'Verbose output') { verbose = true }
+	end.parse!
+
+	# TODO move these into the class
+	croak('you cannot specify both -b and either -l or -w') if !bit_count.nil? && (!max_length.nil? || !word_count.nil?)
+	croak('you cannot specify both -a and -s') if !acrostic.nil? && !prefix.nil?
+	croak('you cannot specify both -a and -b') if !acrostic.nil? && !bit_count.nil?
+	croak('value for -a not consistent with value for -w') if !acrostic.nil? && !word_count.nil? && acrostic.length != word_count
+	croak('you must specify a positive number for -b') if !bit_count.nil? && bit_count <= 0
+	croak('you must specify a positive number for -l') if !max_length.nil? && max_length <= 0
+	croak('you must specify a positive number for -w') if !word_count.nil? && word_count <= 0
+	croak('you must specify a positive number for -r') if !repeat.nil? && repeat <= 0
+	croak('you must specify a number between 0.0 and 1.0 for -S') if prefix_frac <= 0 || prefix_frac > 1.0
+	word_count = acrostic.length if !acrostic.nil?
+	bit_count = DEFAULT_BITS if bit_count.nil? && word_count.nil? && max_length.nil?
+
+	shergar = CorrectHorse.new(words=words, 
+			bit_count=bit_count, max_length=max_length, word_count=word_count,
+			prefix=prefix, prefix_frac=prefix_frac, acrostic=acrostic,
+			caps=caps, digitify=digitify,
+			joiner=joiner)
+
+	(1..repeat).each { puts(shergar.random_password()) }
+
+	if verbose
+		puts('# Output chosen from among %d possible outcomes (%.2f bits of entropy).' %
+			[shergar.possible_outcomes, Math.log(shergar.possible_outcomes,2)])
+		puts('# A choice from the above list can be regarded as having %.2f bits of entropy.' % 
+			Math.log(shergar.possible_outcomes/repeat.to_f,2)) if repeat > 1
 	end
-	opts.on("-w", "--words FILE", String, "file containing Words to choose randomly") do |v|
-		words = File.read(v).scan(/(?:^|(?<=\s))([a-z]+)(?:$|(?=\s))/).flatten
-	end
-end.parse!
 
-word_count = ((bit_count*Math.log(2))/Math.log(words.size)).ceil
-byte_count = ((word_count*Math.log(words.size))/Math.log(256)).ceil
-bits = File.open('/dev/urandom','rb'){|f| f.read(byte_count) }.chars.inject(0){|a,b| a << 8 | b.ord }
-puts (0...word_count).collect{|e| words[(bits / (words.size ** e)) % words.size] }.join(' ')
+end
+
+main() if __FILE__ == $0
 
